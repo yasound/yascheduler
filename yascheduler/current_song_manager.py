1b963704433f4a63460fbcbe44a7fab450b10e7b
matthieu campion <campion.matthieu@gmail.com>
2012-11-09 19:11:05 +0100

    keep songs which have not been reported correctly to send them later

--- yascheduler/current_song_manager.py
+++ yascheduler/current_song_manager.py
@@ -38,22 +38,33 @@ class CurrentSongManager(Thread):
 
         self.logger.info('CurrentSongManager report %d songs' % len(self.current_songs))
 
+        ok = True
+        songs = self.current_songs
+        # clear songs
+        self.flush()
+
         try:
             #  send request...
             url = settings.YASOUND_SERVER + '/api/v1/songs_started/'
-            payload = {'key': settings.SCHEDULER_KEY, 'data': self.current_songs}
+            payload = {'key': settings.SCHEDULER_KEY, 'data': songs}
+
             response = requests.post(url, data=json.dumps(payload))
             result = response.json
             if response.status_code != 200:
+                ok = False
                 self.logger.info('current songs request failed: status code = %d' % response.status_code)
             elif result['success'] == False:
+                ok = False
                 self.logger.info('current songs request error: %s' % result['error'])
 
-            # clear songs
-            self.flush()
-
         except Exception, e:
+            ok = False
             self.logger.info('CurrentSongManager report exception: %s' % str(e))
+
+        if ok == False:
+            # songs have not been correctly sent
+            # reinsert in self.current_songs at index 0
+            self.current_songs[:0] = songs
 
     def store(self, radio_uuid, song_id, play_date=None):
         if play_date == None:
