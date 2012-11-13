52cc94f2ce67d1d4c6d5117210d8d743be0d6de9
matthieu campion <campion.matthieu@gmail.com>
2012-11-13 18:17:41 +0100

    use sortedlist

--- yascheduler/time_event_manager.py
+++ yascheduler/time_event_manager.py
@@ -1,59 +1,77 @@
-import settings
-from pymongo import ASCENDING
 from datetime import datetime
+from time import mktime
+from blist import sortedlist
+
+
+class TimeEvent():
+    EVENT_TYPE_NEW_HOUR_PREPARE = 1
+    EVENT_TYPE_NEW_TRACK_PREPARE = 2
+    EVENT_TYPE_NEW_TRACK_START = 3
+    EVENT_TYPE_TRACK_CONTINUE = 4
+    EVENT_TYPE_CHECK_EXISTING_RADIOS = 5
+    EVENT_TYPE_CHECK_PROGRAMMING = 6
+
+    def __init__(self, event_type, date):
+        self.event_type = event_type
+        self.date = date
+        self.t = mktime(self.date.timetuple()) + self.date.microsecond * 1e-6
+
+    def __eq__(self, other):
+        return self.t == other.t
+
+    def __gt__(self, other):
+        return self.t > other.t
 
 
 class TimeEventManager():
-    EVENT_TYPE_NEW_HOUR_PREPARE = 'prepare_new_hour'
-    EVENT_TYPE_NEW_TRACK_PREPARE = 'prepare_new_track'
-    EVENT_TYPE_NEW_TRACK_START = 'start_new_track'
-    EVENT_TYPE_TRACK_CONTINUE = 'continue_track'
-    EVENT_TYPE_CHECK_EXISTING_RADIOS = 'check_existing_radios'
-    EVENT_TYPE_CHECK_PROGRAMMING = 'check_programming'
 
     def __init__(self):
-        self.time_events = settings.MONGO_DB.scheduler.radios.events
-        self.time_events.ensure_index('radio_uuid')
-        self.time_events.ensure_index('date')
-        self.time_events.ensure_index('type')
+        self.time_events = sortedlist()
 
     def clear(self):
-        self.time_events.drop()
+        self.time_events = sortedlist()
 
     def count(self):
-        return self.time_events.find().count()
+        return len(self.time_events)
 
-    def insert(self, event_doc):
-        self.time_events.insert(event_doc, safe=True)
+    def insert(self, event):
+        self.time_events.add(event)
 
     def pop_past_events(self, ref_date):
-        events_query = {'date': {'$lte': ref_date}}
-        events = list(self.time_events.find(events_query).sort([('date', ASCENDING)]))
-        self.remove_past_events(ref_date)
+        events = []
+        while len(self.time_events) > 0 and self.time_events[0].date <= ref_date:
+            e = self.time_events.pop(0)
+            events.append(e)
         return events
 
     def time_till_next(self, ref_date):
-        next_events = self.time_events.find({'date': {'$gt': ref_date}}, {'date': True}).sort([('date', ASCENDING)]).limit(1)
-        if next_events is None or next_events.count() == 0:
+        if self.count() == 0:
             return None
 
-        next_event = next_events[0]
-        next_date = next_event['date']
+        next_date = self.time_events[0].date
         diff_timedelta = next_date - datetime.now()
         seconds_to_wait = diff_timedelta.days * 86400 + diff_timedelta.seconds + diff_timedelta.microseconds / 1000000.0
         seconds_to_wait = max(seconds_to_wait, 0)
         return seconds_to_wait
 
     def scheduled_radios(self):
-        uuids = frozenset([doc.get('radio_uuid') for doc in self.time_events.find(fields={'radio_uuid': True})])
+        uuids = set()
+        for e in self.time_events:
+            if hasattr(e, 'radio_uuid'):
+                uuids.append(e.radio_uuid)
         return uuids
 
     def remove_radio_events(self, radio_uuid):
-        self.time_events.remove({'radio_uuid': radio_uuid})
+        for i in range(len(self.time_events) - 1, 0, -1):
+            event = self.time_events[i]
+            if hasattr(event, 'radio_uuid') and event.radio_uuid == radio_uuid:
+                self.time_events.pop(i)
 
     def remove_past_events(self, ref_date):
-        self.time_events.remove({'date': {'$lte': ref_date}})
+        self.pop_past_events(ref_date)
 
     def contains_event_type(self, event_type):
-        doc = self.time_events.find_one({'type': event_type}, fields={'type': True})
-        return doc != None
+        for event in self.time_events:
+            if event.event_type == event_type:
+                return True
+        return False
